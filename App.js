4a10734f2225a905cf3e84d8fed00e3cfc293b93
DanAlbertSolis <dansolis9@gmail.com>
2024-09-10 21:53:12 -0700

    Gym Workout Tracker App
    On branch master

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, Button } from "react-native";
+import { StatusBar  } from "expo-status-bar";
+import exercises from "./assets/data/exercises.json";
 
 export default function App() {
+  const exercise = exercises[0];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.exerciseContainer}>
+     <Text style={styles.exerciseName}>{exercise.name}</Text>
+     <Text style={styles.exerciseSubtitle}>
+      {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
+      </Text>
+     </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: "#212121",
     justifyContent: 'center',
+    padding: 20,
+    },
+    exerciseContainer:{
+      backgroundColor: '#fff',
+      padding: 20,
+      borderRadius: 10,
+      gap: 5,
+    },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: '500',
+ 
+  },
+  exerciseSubtitle: {
+    color: 'dimgray',
   },
 });
+
